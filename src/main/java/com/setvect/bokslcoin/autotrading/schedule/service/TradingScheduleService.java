ef2e2f30b273ada607a47a276f183e0d0561fcd5
setvect <setvect@naver.com>
2021-06-09 23:34:21 +0900

    비트코인 분봉 데이터 수집

--- src/main/java/com/setvect/bokslcoin/autotrading/schedule/service/TradingScheduleService.java
+++ src/main/java/com/setvect/bokslcoin/autotrading/schedule/service/TradingScheduleService.java
@@ -12,14 +12,23 @@ import org.springframework.stereotype.Service;
 public class TradingScheduleService {
     private final CoinTrading conCoinTrading;
 
+
+    private final boolean enable;
+
     public TradingScheduleService(
             ApplicationContext context,
-            @Value("${com.setvect.bokslcoin.autotrading.algorithm.name}") String name) {
+            @Value("${com.setvect.bokslcoin.autotrading.algorithm.name}") String name, @Value("com.setvect.bokslcoin.autotrading.enable") String enable) {
+        this.enable = Boolean.getBoolean(enable);
         this.conCoinTrading = (CoinTrading) context.getBean(name);
     }
 
     @Scheduled(fixedRateString = "${com.setvect.bokslcoin.autotrading.schedule.fixedDelay}", initialDelay = 1000)
     public void scheduleCheck() {
+        if (!enable) {
+            log.debug("skip");
+            return;
+        }
+
         conCoinTrading.apply();
     }
 }
--- src/test/java/com/setvect/bokslcoin/autotrading/backtest/Crawling.java
+++ src/test/java/com/setvect/bokslcoin/autotrading/backtest/Crawling.java
@@ -3,7 +3,9 @@ package com.setvect.bokslcoin.autotrading.backtest;
 import com.google.gson.Gson;
 import com.setvect.bokslcoin.autotrading.TestCommonUtil;
 import com.setvect.bokslcoin.autotrading.model.CandleDay;
+import com.setvect.bokslcoin.autotrading.model.CandleMinute;
 import com.setvect.bokslcoin.autotrading.quotation.service.CandleService;
+import com.setvect.bokslcoin.autotrading.util.DateUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +15,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -24,13 +27,14 @@ import java.util.concurrent.TimeUnit;
 @ActiveProfiles("local")
 public class Crawling {
     private static final File SAVE_DIR = new File("./craw-data");
+    private static final File SAVE_DIR_MINUTE = new File("./craw-data/minute");
 
     @Autowired
     private CandleService candleService;
 
     @Test
     public void 일봉수집() throws IOException, InterruptedException {
-        makeSaveDir();
+        makeSaveDir(SAVE_DIR);
         String market = "KRW-BTC";
 
         Gson gson = TestCommonUtil.getGson();
@@ -45,9 +49,9 @@ public class Crawling {
                 break;
             }
             acc.addAll(data);
-            to = data.get(data.size() - 1).getCandleDateTimeKst().minusDays(1);
-            System.out.println(i +" 번째");
-            
+            to = data.get(data.size() - 1).getCandleDateTimeUtc();
+            System.out.println(i + " 번째");
+
             // API 횟수 제한 때문에 딜레이 적용
             TimeUnit.MILLISECONDS.sleep(300);
         }
@@ -58,11 +62,54 @@ public class Crawling {
         System.out.printf("저장: %s%n끝%n", saveFile.getAbsolutePath());
     }
 
-    private void makeSaveDir() {
-        if (SAVE_DIR.exists()) {
+    @Test
+    public void 분봉수집() throws IOException, InterruptedException {
+        makeSaveDir(SAVE_DIR_MINUTE);
+        String market = "KRW-BTC";
+
+        List<CandleMinute> acc = new ArrayList<>();
+        LocalDateTime to = null;
+        int month = LocalDateTime.now(ZoneId.of("UTC")).getMonth().getValue();
+        for (int i = 0; i < 13_000; i++) {
+            List<CandleMinute> data = candleService.getMinute(1, market, 200, to);
+            if (data.isEmpty()) {
+                break;
+            }
+            for (CandleMinute d : data) {
+                LocalDateTime candleDateTimeUtc = d.getCandleDateTimeUtc();
+                if (candleDateTimeUtc.getMonth().getValue() != month) {
+                    saveFileMinute(market, acc);
+                    acc.clear();
+                    month = candleDateTimeUtc.getMonth().getValue();
+                }
+                acc.add(d);
+            }
+            to = data.get(data.size() - 1).getCandleDateTimeUtc();
+            System.out.println(i + " 번째");
+            // API 횟수 제한 때문에 딜레이 적용
+            TimeUnit.MILLISECONDS.sleep(180);
+        }
+        saveFileMinute(market, acc);
+    }
+
+    private void saveFileMinute(String market, List<CandleMinute> acc) throws IOException {
+        if (acc.isEmpty()) {
             return;
         }
-        SAVE_DIR.mkdir();
+        Gson gson = TestCommonUtil.getGson();
+        String fileName = String.format("%s-minute(%s).json", market, DateUtil.format(acc.get(0).getCandleDateTimeUtc(), "yyyy-MM"));
+        File saveFile = new File(SAVE_DIR_MINUTE, fileName);
+        try (FileWriter writer = new FileWriter(saveFile);) {
+            gson.toJson(acc, writer);
+        }
+        System.out.printf("저장: %s%n", saveFile.getAbsolutePath());
+    }
+
+    private void makeSaveDir(File dir) {
+        if (dir.exists()) {
+            return;
+        }
+        dir.mkdirs();
     }
 }
 
